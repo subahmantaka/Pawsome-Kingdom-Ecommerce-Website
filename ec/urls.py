b17e71433d182c69315019f193cf520049482bde
Your Name <suhana.islam@northsouth.edu>
2024-11-29 23:02:38 +0600

    Enhanced UI design and improved existing documentation

--- ec/urls.py
+++ ec/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app.urls')),  # app's urls are included here
+    path('', include('app.urls')),  # app's urls 
 ]
